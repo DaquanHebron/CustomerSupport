d1890ce475fc1fb72ac7fe5c139c4a15b659130f
DaquanHebron <Daquanhebron@gmail.com>
2020-10-20 15:22:07 -0400

    Complete Lab 4

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -50,16 +50,5 @@ public class TicketController {
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
-    public String editTicket(Model model,@PathVariable("id") Long id) {
-        // Code here
-        return "redirect:/"; //Remove this line
-    }
-
-    @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
-    public String deleteTicket(@PathVariable("id") Long id) {
-        // Code here
-        return "redirect:/";
-    }
 
 }
